bbf4589f3c03b89ebdbd3cbe3a55c3726f4a2a18
JafetJimenez <jafetjimenez04@gmail.com>
2024-05-31 00:52:03 -0600

    Se soluciona error en index.js, no reconoce app en pages

--- src/index.js
+++ src/index.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { HashRouter as Router, Route } from 'react-router-dom';
 import './css/normalize.css';
 import './css/style.css';
 import ContactForm from './ContactForm.jsx';
 
 ReactDOM.render(
   <React.StrictMode>
-    <ContactForm />
+    <Router>
+      <Route path="/" exact component={ContactForm} />
+      {/* Agrega más rutas aquí si tienes otros componentes */}
+    </Router>
   </React.StrictMode>,
   document.getElementById('root')
 );
