914a858e2e8a7081287c6b870212a3e3ae432861
JafetJimenez <jafetjimenez04@gmail.com>
2024-05-31 11:24:27 -0600

    se regresa al formato por defecto

--- src/ContactForm.jsx
+++ src/ContactForm.jsx
@@ -7,7 +7,7 @@ function ContactForm() {
     const sendEmail = (e) => {
         e.preventDefault();
 
-        emailjs.sendForm('service_aj30', 'template_aj30', form.current)
+        emailjs.sendForm('service_aj30', 'template_aj30', form.current, 'EoGSI8y0MgnDF5cXL')
         .then(
             (response) => { 
                 console.log('SUCCESS!', response.status, response.text);
